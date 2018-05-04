661cc742b6eb77151bc97eae9f681b63ffe85f1d
draveness <i@draveness.me>
2018-05-04 18:01:38 +0800

    feat: add serializer and serializ function

---
+++ lib/absinthe/serializer.ex
@@ -0,0 +1,136 @@
+defmodule Absinthe.Serializer do
+  alias Absinthe.Language.{
+    Argument,
+    BooleanValue,
+    Document,
+    EnumTypeDefinition,
+    EnumValue,
+    Field,
+    FloatValue,
+    Fragment,
+    FragmentSpread,
+    InlineFragment,
+    IntValue,
+    ListValue,
+    NamedType,
+    NullValue,
+    ObjectField,
+    ObjectValue,
+    OperationDefinition,
+    SelectionSet,
+    StringValue,
+    Variable,
+    VariableDefinition
+  }
+
+  def serialize(%Document{} = document) do
+    document.definitions
+    |> Enum.map(&serialize/1)
+    |> Enum.join("\n")
+  end
+
+  def serialize(
+        %OperationDefinition{
+          name: name,
+          operation: operation,
+          variable_definitions: variable_definitions,
+          selection_set: selection_set
+        } = od
+      ) do
+    variables_string =
+      variable_definitions
+      |> Enum.map(&serialize/1)
+
+    """
+    #{to_string(operation)} #{to_string(name)} #{serialize(selection_set)}
+    """
+  end
+
+  def serialize(%EnumTypeDefinition{}) do
+  end
+
+  def serialize(%Fragment{
+        name: name,
+        selection_set: selection_set,
+        type_condition: type_condition
+      }) do
+    "fragment #{name} on #{serialize(type_condition)} #{serialize(selection_set)}"
+  end
+
+  def serialize(%InlineFragment{
+        selection_set: selection_set,
+        type_condition: type_condition
+      }) do
+    "... on #{serialize(type_condition)} #{serialize(selection_set)}"
+  end
+
+  def serialize(%Argument{name: name, value: value}) do
+    "#{name}: #{serialize(value)}"
+  end
+
+  def serialize(
+        %VariableDefinition{type: type, variable: variable, default_value: default_value} = vd
+      ) do
+    result = "#{serialize(variable)}: #{serialize(type)}"
+    if default_value, do: result <> " = #{serialize(default_value)}", else: result
+  end
+
+  def serialize(%Field{name: name, selection_set: nil}), do: name
+
+  def serialize(%Field{name: name, arguments: arguments, selection_set: selection_set}) do
+    argument_string =
+      if arguments == [] do
+        ""
+      else
+        "(#{arguments |> Enum.map(&serialize/1) |> Enum.join(", ")})"
+      end
+
+    "#{name}#{argument_string} #{serialize(selection_set)}"
+  end
+
+  def serialize(%SelectionSet{selections: selections}) do
+    selection_string =
+      selections
+      |> Enum.map(&serialize/1)
+      |> Enum.join("\n")
+
+    """
+    {
+    #{selection_string}
+    }
+    """
+  end
+
+  def serialize(%ListValue{values: values}) do
+    values_string =
+      values
+      |> Enum.map(&serialize/1)
+      |> Enum.join(", ")
+
+    "[#{values_string}]"
+  end
+
+  def serialize(%ObjectValue{fields: fields}) do
+    fields_string =
+      fields
+      |> Enum.map(&serialize/1)
+      |> Enum.join(", ")
+
+    "{#{fields_string}}"
+  end
+
+  def serialize(%ObjectField{name: name, value: value}) do
+    "#{name}: #{serialize(value)}"
+  end
+
+  def serialize(%Variable{name: name}), do: "$#{name}"
+  def serialize(%NamedType{name: name}), do: name
+  def serialize(%IntValue{value: value}), do: value
+  def serialize(%FloatValue{value: value}), do: value
+  def serialize(%StringValue{value: value}), do: value
+  def serialize(%BooleanValue{value: value}), do: value
+  def serialize(%EnumValue{value: value}), do: value
+  def serialize(%EnumValue{value: value}), do: value
+  def serialize(%NullValue{}), do: "null"
+  def serialize(%FragmentSpread{name: name}), do: "...#{name}"
+end
---
+++ test/absinthe/serializer_test.exs
@@ -0,0 +1,240 @@
+defmodule Absinthe.SerializerTest do
+  use Absinthe.Case, async: true
+
+  alias Absinthe.Serializer
+
+  describe "serialize/1" do
+    test "serialize variables definition" do
+      assert %Absinthe.Language.VariableDefinition{
+               default_value: nil,
+               loc: %{start_line: 1},
+               type: %Absinthe.Language.NamedType{
+                 loc: %{start_line: 1},
+                 name: "Episode"
+               },
+               variable: %Absinthe.Language.Variable{
+                 loc: %{start_line: 1},
+                 name: "episode"
+               }
+             }
+             |> Serializer.serialize() == "$episode: Episode"
+    end
+
+    test "serialize selection sets" do
+      assert %Absinthe.Language.SelectionSet{
+               loc: %{end_line: 7, start_line: 2},
+               selections: [
+                 %Absinthe.Language.Field{
+                   alias: nil,
+                   arguments: [],
+                   directives: [],
+                   loc: %{start_line: 3},
+                   name: "name",
+                   selection_set: nil
+                 },
+                 %Absinthe.Language.Field{
+                   alias: nil,
+                   arguments: [],
+                   directives: [],
+                   loc: %{start_line: 4},
+                   name: "friends",
+                   selection_set: %Absinthe.Language.SelectionSet{
+                     loc: %{end_line: 6, start_line: 4},
+                     selections: [
+                       %Absinthe.Language.Field{
+                         alias: nil,
+                         arguments: [],
+                         directives: [],
+                         loc: %{start_line: 5},
+                         name: "name",
+                         selection_set: nil
+                       }
+                     ]
+                   }
+                 }
+               ]
+             }
+             |> Serializer.serialize() == "{\nname\nfriends {\nname\n}\n\n}\n"
+    end
+
+    test "serialize field" do
+      assert %Absinthe.Language.Field{
+               alias: nil,
+               arguments: [
+                 %Absinthe.Language.Argument{
+                   loc: %{start_line: 2},
+                   name: "episode",
+                   value: %Absinthe.Language.Variable{
+                     loc: %{start_line: 2},
+                     name: "episode"
+                   }
+                 }
+               ],
+               directives: [],
+               loc: %{start_line: 2},
+               name: "hero",
+               selection_set: %Absinthe.Language.SelectionSet{
+                 loc: %{end_line: 7, start_line: 2},
+                 selections: [
+                   %Absinthe.Language.Field{
+                     alias: nil,
+                     arguments: [],
+                     directives: [],
+                     loc: %{start_line: 3},
+                     name: "name",
+                     selection_set: nil
+                   },
+                   %Absinthe.Language.Field{
+                     alias: nil,
+                     arguments: [],
+                     directives: [],
+                     loc: %{start_line: 4},
+                     name: "friends",
+                     selection_set: %Absinthe.Language.SelectionSet{
+                       loc: %{end_line: 6, start_line: 4},
+                       selections: [
+                         %Absinthe.Language.Field{
+                           alias: nil,
+                           arguments: [],
+                           directives: [],
+                           loc: %{start_line: 5},
+                           name: "name",
+                           selection_set: nil
+                         }
+                       ]
+                     }
+                   }
+                 ]
+               }
+             }
+             |> Serializer.serialize() ==
+               "hero(episode: $episode) {\nname\nfriends {\nname\n}\n\n}\n"
+    end
+
+    test "serialize list value" do
+      assert %Absinthe.Language.ListValue{
+               loc: %{start_line: 1},
+               values: [
+                 %Absinthe.Language.IntValue{
+                   loc: %{start_line: 1},
+                   value: 1
+                 },
+                 %Absinthe.Language.IntValue{
+                   loc: %{start_line: 1},
+                   value: 2
+                 },
+                 %Absinthe.Language.IntValue{
+                   loc: %{start_line: 1},
+                   value: 3
+                 }
+               ]
+             }
+             |> Serializer.serialize() == "[1, 2, 3]"
+    end
+
+    test "serialize object value" do
+      assert %Absinthe.Language.ObjectValue{
+               fields: [
+                 %Absinthe.Language.ObjectField{
+                   loc: %{start_line: 1},
+                   name: "uuid",
+                   value: %Absinthe.Language.IntValue{
+                     loc: %{start_line: 1},
+                     value: 1
+                   }
+                 }
+               ],
+               loc: %{start_line: 1}
+             }
+             |> Serializer.serialize() == "{uuid: 1}"
+    end
+
+    test "serialzie fragment spread" do
+      assert %Absinthe.Language.FragmentSpread{
+               directives: [],
+               loc: %{start_line: 7},
+               name: "NameAndAppearances"
+             }
+             |> Serializer.serialize() == "...NameAndAppearances"
+    end
+
+    test "serialize fragment" do
+      assert %Absinthe.Language.Fragment{
+               directives: [],
+               loc: %{start_line: 13},
+               name: "NameAndAppearances",
+               selection_set: %Absinthe.Language.SelectionSet{
+                 loc: %{end_line: 16, start_line: 13},
+                 selections: [
+                   %Absinthe.Language.Field{
+                     alias: nil,
+                     arguments: [],
+                     directives: [],
+                     loc: %{start_line: 14},
+                     name: "name",
+                     selection_set: nil
+                   },
+                   %Absinthe.Language.Field{
+                     alias: nil,
+                     arguments: [],
+                     directives: [],
+                     loc: %{start_line: 15},
+                     name: "appearsIn",
+                     selection_set: nil
+                   }
+                 ]
+               },
+               type_condition: %Absinthe.Language.NamedType{
+                 loc: %{start_line: 13},
+                 name: "Character"
+               }
+             }
+             |> Serializer.serialize() ==
+               "fragment NameAndAppearances on Character {\nname\nappearsIn\n}\n"
+    end
+
+    test "serialiez default variables" do
+      assert %Absinthe.Language.VariableDefinition{
+               default_value: %Absinthe.Language.StringValue{
+                 loc: %{start_line: 1},
+                 value: "JEDI"
+               },
+               loc: %{start_line: 1},
+               type: %Absinthe.Language.NamedType{
+                 loc: %{start_line: 1},
+                 name: "Episode"
+               },
+               variable: %Absinthe.Language.Variable{
+                 loc: %{start_line: 1},
+                 name: "episode"
+               }
+             }
+             |> Serializer.serialize() == "$episode: Episode = JEDI"
+    end
+
+    test "serialize inline serializer" do
+      assert %Absinthe.Language.InlineFragment{
+               directives: [],
+               loc: %{start_line: 3},
+               selection_set: %Absinthe.Language.SelectionSet{
+                 loc: %{end_line: 5, start_line: 3},
+                 selections: [
+                   %Absinthe.Language.Field{
+                     alias: nil,
+                     arguments: [],
+                     directives: [],
+                     loc: %{start_line: 4},
+                     name: "primaryFunction",
+                     selection_set: nil
+                   }
+                 ]
+               },
+               type_condition: %Absinthe.Language.NamedType{
+                 loc: %{start_line: 3},
+                 name: "Droid"
+               }
+             }
+             |> Serializer.serialize() == "... on Droid {\nprimaryFunction\n}\n"
+    end
+  end
+end
